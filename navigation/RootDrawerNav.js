1205cb1332903383084df516759a7a5e5a5f3156
Shreyansh-Agrawal <shreyansh.brbd@gmail.com>
2022-03-08 11:55:52 +0530

    Added more styling

--- navigation/RootDrawerNav.js
+++ navigation/RootDrawerNav.js
@@ -1,45 +1,68 @@
-import * as React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-import HomeScreen from '../src/screens/Home/HomeScreen';
-import Profile from '../src/screens/Profile/profile';
-import Settings from '../src/screens/Settings';
+import HomeScreen from "../src/screens/Home/HomeScreen";
+import Profile from "../src/screens/Profile/profile";
+import Settings from "../src/screens/Settings";
 
 const Drawer = createDrawerNavigator();
 
-export default function RootDrawerNav(){
-    return (
-        <Drawer.Navigator>
-          <Drawer.Screen name="Home" component={HomeScreen} options={{
-          title: 'My Home',
+export default function RootDrawerNav() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "#000",
+          width: 240,
+        },
+        drawerLabelStyle:{
+          fontSize:18,
+          color:"#fff"
+        }
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "My Home",
           headerStyle: {
-            backgroundColor: '#000',
+            backgroundColor: "#000",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
           },
-        }}/>
-          <Drawer.Screen name="Profile" component={Profile} options={{
-          title: 'My Profile',
+        }}
+      />
+      <Drawer.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          title: "My Profile",
           headerStyle: {
-            backgroundColor: '#000',
+            backgroundColor: "#000",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
           },
-        }}/>
-          <Drawer.Screen name="Settings" component={Settings} options={{
-          title: 'My Settings',
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          title: "My Settings",
           headerStyle: {
-            backgroundColor: '#000',
+            backgroundColor: "#000",
           },
-          headerTintColor: '#fff',
+          headerTintColor: "#fff",
           headerTitleStyle: {
-            fontWeight: 'bold',
+            fontWeight: "bold",
           },
-        }}/>
-        </Drawer.Navigator>
-      );
+        }}
+      />
+    </Drawer.Navigator>
+  );
 }
--- src/screens/Profile/profile.js
+++ src/screens/Profile/profile.js
@@ -1,16 +1,46 @@
-import * as React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import User1 from './User1';
-import User2 from './User2';
+import User1 from "./User1";
+import User2 from "./User2";
 
 const Tab = createBottomTabNavigator();
 
 export default function Profile() {
+  const screenOptions = {
+    tabBarStyle: {
+      backgroundColor: "#000",
+      height: 80,
+      borderRadius: 40,
+      margin: 10,
+    },
+    tabBarItemStyle: {
+      backgroundColor: "#fff",
+      margin: 10,
+      borderRadius: 30,
+    },
+  };
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="user1" component={User1} options={{headerShown: false, tabBarBadge: 3}}/>
-      <Tab.Screen name="user2" component={User2} options={{headerShown: false, tabBarBadge: 1}}/>
+    <Tab.Navigator
+      {...{ screenOptions }}
+
+      tabBarOptions={{
+        activeTintColor: "#000",
+        inactiveTintColor: "#555",
+        labelStyle: { fontSize: 20 },
+      }}
+    >
+      <Tab.Screen
+        name="user1"
+        component={User1}
+        options={{ headerShown: false, tabBarBadge: 3, title:"USER 1" }}
+      />
+      <Tab.Screen
+        name="user2"
+        component={User2}
+        options={{ headerShown: false, tabBarBadge: 1, title:"USER 1" }}
+      />
     </Tab.Navigator>
   );
 }
